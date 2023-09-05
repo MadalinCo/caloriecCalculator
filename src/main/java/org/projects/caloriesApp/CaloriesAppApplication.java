81e3ecd42c93eef1fec8c5abe14739c07264e8b5
MadalinCo <colibamadalin@gmail.com>
2023-09-05 14:55:55 +0300

    first commit

---
+++ src/main/java/org/projects/caloriesApp/CaloriesAppApplication.java
@@ -0,0 +1,13 @@
+package org.projects.caloriesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesAppApplication.class, args);
+	}
+
+}
